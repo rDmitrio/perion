31024e6345a504be3d03d3fca8c8e6aefc9492b6
r.dima <dimkarm2@gmail.com>
2020-04-27 11:38:05 +0300

    final changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <header>
+      <img alt="logo" src="@/assets/logo.png" @click="$router.push('/')">
+    </header>
     <router-view/>
   </div>
 </template>
@@ -8,7 +11,7 @@
 body {
   padding: 0;
   margin: 0;
-  background-color: #3a3a3a;
+  background-color: #2e2e2e;
 }
 
 #app {
@@ -16,7 +19,17 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
+
+  header {
+    text-align: center;
+    padding: 40px 20px;
+
+    img {
+      width: 200px;
+      cursor: pointer;
+    }
+  }
 
 }
 
